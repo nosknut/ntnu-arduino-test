ed1ae9158beda29b7564a9c611e37cd1008c8088
Knut Ola Nøsen <nosknut@gmail.com>
2021-08-23 14:40:25 +0200

    Cross

---
+++ Cross.ino
@@ -0,0 +1,92 @@
+bool OUTPUT = true;
+bool HIGH = true;
+bool LOW = false;
+void pinMode(int pin, bool mode){
+
+};
+
+void digitalWrite(int pin, bool mode){
+
+};
+
+class Light
+{
+public:
+  static enum State { INACTIVE = 0,
+                      ACTIVATING = 1,
+                      ACTIVE = 2,
+                      DEACTIVATING = 3,
+  };
+
+private:
+  int red, yellow, green;
+  State state = State::INACTIVE;
+
+public:
+  Light(int red, int yellow, int green)
+  {
+    this->red = red;
+    this->yellow = yellow;
+    this->green = green;
+  }
+
+  void setup()
+  {
+    pinMode(red, OUTPUT);
+    pinMode(yellow, OUTPUT);
+    pinMode(green, OUTPUT);
+  }
+  void update()
+  {
+    updateLightState();
+  }
+
+private:
+  void setLightOutputs(bool r, bool y, bool g)
+  {
+    digitalWrite(red, r);
+    digitalWrite(yellow, y);
+    digitalWrite(green, g);
+  }
+  void updateLightState()
+  {
+    switch (state)
+    {
+    case State::INACTIVE:
+      setLightOutputs(LOW, LOW, LOW);
+      return;
+    case State::ACTIVATING:
+      setLightOutputs(HIGH, HIGH, LOW);
+      return;
+    case State::ACTIVE:
+      setLightOutputs(LOW, LOW, HIGH);
+      return;
+    case State::DEACTIVATING:
+      setLightOutputs(LOW, HIGH, LOW);
+      return;
+    default:
+      return;
+    }
+  }
+};
+
+Light a(1, 2, 3);
+Light b(4, 5, 6);
+Light c(7, 8, 9);
+Light d(10, 11, 12);
+
+void setup()
+{
+  a.setup();
+  b.setup();
+  c.setup();
+  d.setup();
+}
+
+void loop()
+{
+  a.update();
+  b.update();
+  c.update();
+  d.update();
+}
